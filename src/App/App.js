ff4cda7919a25c43c4dd96deb48a87fe2d9a1eb1
Matt Saqueli <msaqueli22@turing.edu>
2023-05-30 10:27:12 -0500

    Fetch data from API

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => response.json())
+    .then(reservationData => {
+      this.setState({
+        reservations: reservationData
+      })
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
