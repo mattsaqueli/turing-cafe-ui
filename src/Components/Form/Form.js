be1c5e6a1e7b2df9c6f0d49da9fa22909be21d0d
Matt Saqueli <msaqueli22@turing.edu>
2023-05-30 10:57:54 -0500

    Create Form inputs

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: "",
+      date: "",
+      time: "",
+      number: ""
+    }
+  }
+
+  render() {
+    return (
+      <form className="form-input">
+        <input className="name-input" type="text" placeholder="Name" value={this.state.name}></input>
+        <input className="date-input" type="text" placeholder="Date" value={this.state.date}></input>
+        <input className="time-input" type="text" placeholder="Time" value={this.state.time}></input>
+        <input className="guest-input" type="number" placeholder="Number of guests" value={this.state.number}></input>
+        <button>Make A Reservation</button>
+      </form>
+    )
+  }
+}
